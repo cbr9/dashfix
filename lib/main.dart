ec12256bc80853bee92f62e17a341365096c9ef0
cbr9 <cabero96@protonmail.com>
2023-03-29 19:53:02 +0200

    add db and test connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dashfix/pages/posts_page.dart';
 import 'package:dashfix/post_bank.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const DashFix());
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  final users = db.collection('users');
+  print(users);
 }
 
 class DashFix extends StatelessWidget {
